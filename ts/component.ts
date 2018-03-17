6452ad5316c12bbfb08ddf42f53475da6e61359b
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2018-03-17 20:29:25 +0000

    Starting point for CPU get methods done. Got temp IP banned so lets hope that this wasnt all in vain

--- ts/component.ts
+++ ts/component.ts
@@ -10,33 +10,64 @@ export abstract class Component {
     samples: number = 0
 
     abstract isValid(old: Component): boolean
-    abstract get(): Component
+    abstract get(body: HTMLBodyElement, n: Nightmare): Component
 }
 
 export class CPU extends Component {
-    cores: string = ""
+    cores: string 
     averages: string[]
-    performance: string[]
     subResults: string[]
+    performance: string[]
 
     constructor() {
         super()
+        this.cores = ""
         this.averages = new Array(3)
         this.performance = new Array(3)
         this.subResults = new Array(9)
     }
     
-    isValid(old: Component): boolean {
+    isValid(old: CPU): boolean {
         return true
     }
 
-    get(): CPU {
-
+    get(body: HTMLBodyElement, n: Nightmare): CPU {
+        this.getCores(body, n)
+        this.getAverages(body, n)
+        this.getSubResults(body)
         return this
     }
 
-    getCores(n: Nightmare) {
-        
+    getCores(body: HTMLBodyElement, n: Nightmare) {
+        let sel = '.cmp-cpt.tallp.cmp-cpt-l'
+        n.exists(sel, (exists: boolean) => {
+            if(exists) {
+                this.cores = body.querySelector(sel)!.textContent!
+                return
+            }
+            this.cores = ""
+        })
+    }
+
+    getAverages(body: HTMLBodyElement, n: Nightmare) {
+        for(var i = 0; i < 3; i++) {
+            let selGreen  = '.para-m-t.uc-table.table-no-border > thead > tr > td:nth-child(${i+3}) .mcs-caption.pgbg'
+            let selYellow = '.para-m-t.uc-table.table-no-border > thead > tr > td:nth-child(${i+3}) .mcs-caption.pybg'
+            let selRed    = '.para-m-t.uc-table.table-no-border > thead > tr > td:nth-child(${i+3}) .mcs-caption.prbg'
+            n.exists(`${selGreen}, ${selYellow}, ${selRed}`, (exists: boolean) => {
+                if(exists) {
+                    this.averages[i] = body.querySelector(`${selGreen}, ${selYellow}, ${selRed}`)!.textContent!
+                    return
+                } 
+                console.log("nothing found???")
+            })
+        }
+    }
+
+    getSubResults(body: HTMLBodyElement) {
+        body.querySelectorAll('.mcs-hl-col').forEach((tag: Element, i: number, _: NodeListOf<Element>) => {
+            this.subResults[i] = tag.textContent!
+        }, this)
     }
 }
 
@@ -46,11 +77,75 @@ export class GPU extends Component {
         
     }
 
-    isValid(old: Component): boolean {
+    isValid(old: GPU): boolean {
         return true
     }
 
-    get(): GPU {
+    get(body: HTMLBodyElement, n: Nightmare): GPU {
+
+        return this
+    }
+}
+
+export class RAM extends Component {
+    constructor() {
+        super()
+    }
+
+    isValid(old: RAM): boolean {
+
+        return true
+    }
+
+    get(body: HTMLBodyElement, n: Nightmare): RAM {
+        
+        return this
+    }
+}
+
+export class SSD extends Component {
+    constructor() {
+        super()
+    }
+
+    isValid(old: SSD): boolean {
+
+        return true
+    }
+
+    get(body: HTMLBodyElement, n: Nightmare): SSD {
+
+        return this
+    }
+}
+
+export class HDD extends Component {
+    constructor() {
+        super()
+    }
+
+    isValid(old: HDD): boolean {
+        
+        return true
+    }
+
+    get(body: HTMLBodyElement, n: Nightmare): HDD {
+
+        return this
+    }
+}
+
+export class USB extends Component {
+    constructor() {
+        super()
+    }
+
+    isValid(old: USB): boolean {
+
+        return true
+    }
+
+    get(body: HTMLBodyElement, n: Nightmare): USB {
 
         return this
     }
--- ts/main.ts
+++ ts/main.ts
@@ -1,21 +1,34 @@
 import * as Nightmare from 'nightmare'
-import { CPU, GPU, Component } from './component'
+import { CPU, GPU, RAM, SSD, HDD, USB, Component } from './component'
 
-let nightmare = new Nightmare({show: false})
+let nightmare = new Nightmare({show: true})
 
-async function get() {
-    try {
-        let result = await nightmare
-            .goto("http://cpu.userbenchmark.com/Intel-Core-i7-8700K/Rating/3937")
-            .evaluate(() => { return document.querySelector('html')!.innerHTML })
-            .end();
-        return result
-    } catch(e) {
-        console.log(e)
-        return undefined
-    }
+let c = new CPU()
+c.url = "http://cpu.userbenchmark.com/Intel-Core-i7-8700K/Rating/3937"
+
+try {
+    nightmare
+        .goto(c.url)
+        .evaluate((): string => {
+            return window.location.toString()
+        }, (res: string) => {
+            console.log(res)
+        })
+        .wait('body')
+        .evaluate((): HTMLBodyElement => { 
+            return document.querySelector('body')!
+        }, (res: HTMLBodyElement) => {
+            console.dir(res)
+            console.log(res)
+            c.get(res, nightmare)
+        })
+        .end()
+        .then(() => {
+            
+        })
+        .catch((e) => {
+            console.log(e)
+        })
+} catch(e) {
+    console.log(e)
 }
-
-(async function() {
-    console.log(await get())
-}())
